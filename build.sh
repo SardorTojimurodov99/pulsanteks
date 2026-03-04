475273ed352ddb50e896b63f2f702b26e4c9cb1b
pulsanteks <tojimurodovsardor77@gmail.com>
2026-03-04 21:27:24 +0300

    add dj-database-url1

--- build.sh
+++ build.sh
@@ -2,11 +2,19 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py migrate
 
-# Superuser (agar env o'zgaruvchilar qo'yilgan bo'lsa)
-if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_PASSWORD" && -n "$DJANGO_SUPERUSER_EMAIL" ]]; then
-  python manage.py createsuperuser --noinput || true
-fi
+# ---- Create / update admin user (Render) ----
+python manage.py shell -c "import os; \
+from django.contrib.auth import get_user_model; \
+User=get_user_model(); \
+u=os.environ.get('ADMIN_USERNAME'); \
+e=os.environ.get('ADMIN_EMAIL',''); \
+p=os.environ.get('ADMIN_PASSWORD'); \
+assert u and p, 'ADMIN_USERNAME/ADMIN_PASSWORD env yoq'; \
+obj,created=User.objects.get_or_create(username=u, defaults={'email': e, 'is_staff': True, 'is_superuser': True}); \
+obj.email=e or obj.email; \
+obj.is_staff=True; obj.is_superuser=True; \
+obj.set_password(p); obj.save(); \
+print('ADMIN_OK', created)"
