112f7e2953335daaf9fb344f31d821b08e170190
pulsanteks <tojimurodovsardor77@gmail.com>
2026-03-04 20:52:14 +0300

    add dj-database-url1

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+# Superuser (agar env o'zgaruvchilar qo'yilgan bo'lsa)
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_PASSWORD" && -n "$DJANGO_SUPERUSER_EMAIL" ]]; then
+  python manage.py createsuperuser --noinput || true
+fi
