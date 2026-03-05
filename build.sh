0c563a2bce67873e9aebd7f50ade7116147cff29
pulsanteks <tojimurodovsardor77@gmail.com>
2026-03-05 18:32:17 +0300

    add dj-database-url1

--- build.sh
+++ build.sh
@@ -6,15 +6,21 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 # ---- Create / update admin user (Render) ----
-python manage.py shell -c "import os; \
-from django.contrib.auth import get_user_model; \
-User=get_user_model(); \
-u=os.environ.get('DJANGO_SUPERUSER_USERNAME') or os.environ.get('ADMIN_USERNAME'); \
-e=os.environ.get('DJANGO_SUPERUSER_EMAIL','') or os.environ.get('ADMIN_EMAIL',''); \
-p=os.environ.get('DJANGO_SUPERUSER_PASSWORD') or os.environ.get('ADMIN_PASSWORD'); \
-assert u and p, 'SUPERUSER env yoq'; \
-obj,created=User.objects.get_or_create(username=u, defaults={'email': e, 'is_staff': True, 'is_superuser': True}); \
-obj.email=e or obj.email; \
-obj.is_staff=True; obj.is_superuser=True; \
-obj.set_password(p); obj.save(); \
-print('ADMIN_OK', created)"
+python manage.py shell -c "import os
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+u=os.environ.get('DJANGO_SUPERUSER_USERNAME')
+p=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+e=os.environ.get('DJANGO_SUPERUSER_EMAIL','')
+
+assert u and p, 'DJANGO_SUPERUSER_USERNAME/PASSWORD env yoq'
+
+obj, _ = User.objects.get_or_create(username=u, defaults={'email': e})
+obj.email = e or obj.email
+obj.is_staff = True
+obj.is_superuser = True
+obj.set_password(p)
+obj.save()
+print('ADMIN_RESET_OK', u)
+"
