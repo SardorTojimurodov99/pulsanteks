b067f968cbd77b63363f6c0c6f24615a933b6878
pulsanteks <tojimurodovsardor77@gmail.com>
2026-03-07 18:40:26 +0300

    add dj-database-url1

--- config/settings.py
+++ config/settings.py
@@ -98,13 +98,12 @@ if DATABASE_URL:
     }
 else:
     # Local postgres uchun
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
 # =========================
 # PASSWORD VALIDATORS
 # =========================
