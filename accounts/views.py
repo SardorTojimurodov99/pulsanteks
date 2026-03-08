da2a2da00694f4c675f1097b45860e2e092ddfa4
pulsanteks <tojimurodovsardor77@gmail.com>
2026-03-08 13:00:11 +0300

    mukkamal quest

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LoginView, LogoutView
-
-
-class UserLoginView(LoginView):
-    template_name = "accounts/login.html"
-
-
-class UserLogoutView(LogoutView):
-    pass
